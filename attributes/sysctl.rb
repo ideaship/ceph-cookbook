951940f437ec0c0fa3e614527be7c6d1367a11ec
Jens Rosenboom <j.rosenboom@x-ion.de>
2015-12-03 13:43:32 +0100

    Add sysctl for vm.min_free_kbytes

--- attributes/sysctl.rb
+++ attributes/sysctl.rb
@@ -1,2 +1,3 @@
 # need to include sysctl recipe in addition to setting this attribute
 node.default['sysctl']['params']['vm']['swappiness'] = 1
+node.default['sysctl']['params']['vm']['min_free_kbytes'] = 2_097_152

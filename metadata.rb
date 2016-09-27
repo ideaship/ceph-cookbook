9f6b8f644a80025d6b35c1098e04f8ea17b9dce3
Jan Klare <j.klare@cloudbau.de>
2016-09-27 11:21:24 +0200

    fixing radosgw log path and chaning maintainer in metadata

--- metadata.rb
+++ metadata.rb
@@ -1,10 +1,10 @@
 name 'ceph'
-maintainer 'Chris Jones'
-maintainer_email 'cjones303@bloomberg.net, cjones@cloudm2.com'
+maintainer 'Jan Klare'
+maintainer_email 'j.klare@cloudbau.de'
 license 'Apache 2.0'
 description 'Installs/Configures the Ceph distributed filesystem'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version '1.1.0'
+version '1.1.1'
 
 depends	'apache2', '>= 1.1.12'
 depends 'apt'

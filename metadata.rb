5ebac805b70bface81c25511c0b13809ef901e6a
Jens Harbott <j.harbott@x-ion.de>
2017-12-07 11:57:00 +0000

    Add netaddr gem to metadata
    Needed for chef-client 13 run to succeed.

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'j.klare@cloudbau.de'
 license 'Apache 2.0'
 description 'Installs/Configures the Ceph distributed filesystem'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version '1.4.0'
+version '1.5.0'
 
 depends	'apache2', '>= 1.1.12'
 depends 'apt'
@@ -15,3 +15,5 @@ depends 'openstack-common'
 
 source_url 'https://github.com/cloudbau/ceph-cookbook' if respond_to?(:source_url)
 issues_url 'https://github.com/cloudbau/ceph-cookbook/issues' if respond_to?(:issues_url)
+
+gem 'netaddr'

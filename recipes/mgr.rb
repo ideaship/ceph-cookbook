bc6d917e79f2e0a280c0d3156c8cc9dafe051de5
Jens Harbott <j.harbott@x-ion.de>
2018-01-25 10:11:19 +0000

    Add receipe to deploy ceph-mgr

---
+++ recipes/mgr.rb
@@ -0,0 +1,83 @@
+#
+# Author:: Kyle Bader <kyle.bader@dreamhost.com>
+# Author:: Dr. Jens Harbott <j.harbott@x-ion.de>
+# Cookbook Name:: ceph
+# Recipe:: mgr
+#
+# Copyright 2011, DreamHost Web Hosting
+# Copyright 2018, x-ion GmbH
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+include_recipe 'ceph'
+
+cluster = 'ceph'
+
+directory "/var/lib/ceph/mgr/#{cluster}-#{node['hostname']}" do
+  owner 'root'
+  group 'root'
+  mode 00755
+  recursive true
+  action :create
+end
+
+mgr_user = "mgr.#{node['hostname']}"
+
+ceph_user mgr_user do
+  caps('osd' => 'allow *', 'mon' => 'allow profile mgr', 'mds' => 'allow *')
+  key ceph_secret(mgr_user)
+  action :create
+end
+
+ceph_client mgr_user do
+  keyname mgr_user
+  filename "/var/lib/ceph/mgr/#{cluster}-#{node['hostname']}/keyring"
+  owner 'ceph'
+  group 'ceph'
+end
+
+file "/var/lib/ceph/mgr/#{cluster}-#{node['hostname']}/done" do
+  owner 'root'
+  group 'root'
+  mode 00644
+end
+
+service_type = node['ceph']['init_style']
+
+filename = case service_type
+           when 'systemd'
+             'systemd'
+           when 'upstart'
+             'upstart'
+           else
+             'sysvinit'
+           end
+file "/var/lib/ceph/mgr/#{cluster}-#{node['hostname']}/#{filename}" do
+  owner 'root'
+  group 'root'
+  mode 00644
+end
+
+service 'ceph_mgr' do
+  case service_type
+  when 'upstart'
+    service_name 'ceph-mgr-all-starter'
+    provider Chef::Provider::Service::Upstart
+  when 'systemd'
+    service_name "ceph-mgr@#{node['hostname']}"
+  else
+    service_name 'ceph'
+  end
+  action [:enable, :start]
+  supports restart: true
+end
